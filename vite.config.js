51de4303a6c7d3399291ac42c359f3ae678f7fcd
Mino <minho00123@gmail.com>
2025-07-27 22:32:53 +0900

    feat: Axios 설정

--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,7 @@ export default defineConfig({
       "@hooks": path.resolve(__dirname, "./src/hooks"),
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@services": path.resolve(__dirname, "./src/services"),
+      "@stores": path.resolve(__dirname, "./src/stores"),
       "@styles": path.resolve(__dirname, "./src/styles"),
       "@utils": path.resolve(__dirname, "./src/utils"),
     },
