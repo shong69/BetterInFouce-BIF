434c1515e19b0bb9694a92d3a06b6b87d97d5148
Mino <minho00123@gmail.com>
2025-07-27 22:39:02 +0900

    feat: React Router 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <h1 className="text-primary text-3xl font-bold underline">Hello World</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
