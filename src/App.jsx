8258fd4164755fb9faf38b23aa9615836b950c61
Mino <minho00123@gmail.com>
2025-07-27 21:44:34 +0900

    feat: TaiwindCSS 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <h1 className="text-primary text-3xl font-bold underline">Hello World</h1>
+  );
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -6,7 +7,7 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     host: "localhost",
     port: 3000,
