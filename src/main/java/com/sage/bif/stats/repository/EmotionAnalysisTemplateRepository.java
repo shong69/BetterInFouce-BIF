01d7251ca3e00d290ee5149e92041061b982ddeb
99MotiveU <dongi292@hanmail.net>
2025-07-31 12:45:13 +0900

    feat: 통계 보완 및 보호자 조언 제공 구현

--- src/main/java/com/sage/bif/stats/repository/EmotionAnalysisTemplateRepository.java
+++ src/main/java/com/sage/bif/stats/repository/EmotionAnalysisTemplateRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -21,6 +19,4 @@ public interface EmotionAnalysisTemplateRepository extends JpaRepository<Emotion
 
     @Query("SELECT t FROM EmotionAnalysisTemplate t ORDER BY RAND() LIMIT 1")
     Optional<EmotionAnalysisTemplate> findRandomTemplate();
-
-    List<EmotionAnalysisTemplate> findAll();
-} 
+}
