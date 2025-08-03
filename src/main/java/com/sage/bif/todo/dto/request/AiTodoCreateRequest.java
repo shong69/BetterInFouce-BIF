1581678f8cc1c07df5bf428ddc907d7097ca5b34
Mino <minho00123@gmail.com>
2025-08-03 15:16:28 +0900

    feat: AI 관련 DTO 생성

--- src/main/java/com/sage/bif/todo/dto/request/AiTodoCreateRequest.java
+++ src/main/java/com/sage/bif/todo/dto/request/AiTodoCreateRequest.java
@@ -1,5 +1,7 @@
 package com.sage.bif.todo.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +13,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AiTodoCreateRequest {
 
+    @NotBlank(message = "사용자 입력은 필수입니다")
+    @Size(min = 1, max = 1000, message = "사용자 입력은 1-1000자 사이여야 합니다")
     private String userInput;
 
 }
