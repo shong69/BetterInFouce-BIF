d3b1f0551b62b301cc1f0570b22d511f56f63316
Mino <minho00123@gmail.com>
2025-08-04 11:28:31 +0900

    refactor: 배경색 삭제

--- src/components/ui/DateBox.jsx
+++ src/components/ui/DateBox.jsx
@@ -8,7 +8,7 @@ export default function DateBox() {
   }
 
   return (
-    <div className="flex items-center justify-between bg-white px-5 py-5">
+    <div className="flex items-center justify-between px-5 py-5">
       <div className="text-black-600 text-[13px] font-medium tracking-wide">
         {getCurrentDate()}
       </div>
