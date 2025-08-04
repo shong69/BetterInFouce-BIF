03efd51c7b1b814a359d18a7ee4a90bf22264a62
Mino <minho00123@gmail.com>
2025-08-04 11:32:32 +0900

    feat: 색깔 및 날짜 관련 util 함수 추가

--- src/components/ui/DateBox.jsx
+++ src/components/ui/DateBox.jsx
@@ -1,12 +1,6 @@
-export default function DateBox() {
-  function getCurrentDate() {
-    const now = new Date();
-    const month = now.getMonth() + 1;
-    const date = now.getDate();
-    const day = ["일", "월", "화", "수", "목", "금", "토"][now.getDay()];
-    return `${month}월 ${date}일 ${day}요일`;
-  }
+import { getCurrentDate } from "../../utils/dateUtils";
 
+export default function DateBox() {
   return (
     <div className="flex items-center justify-between px-5 py-5">
       <div className="text-black-600 text-[13px] font-medium tracking-wide">
