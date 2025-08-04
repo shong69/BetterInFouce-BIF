d2380de73276e8f1b7bb7040c3b714c5d4a202ff
Mino <minho00123@gmail.com>
2025-08-04 11:28:58 +0900

    feat: 공통 뒤로가기 버튼 생성

---
+++ src/components/ui/BackButton.jsx
@@ -0,0 +1,24 @@
+import { HiChevronLeft } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
+
+export default function BackButton({ title = "뒤로가기", onClick }) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    } else {
+      navigate(-1);
+    }
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className="text-black-600 flex items-center text-[20px] font-bold transition-colors hover:text-gray-900"
+    >
+      <HiChevronLeft size={20} />
+      <span className="mr-2">{title}</span>
+    </button>
+  );
+}
