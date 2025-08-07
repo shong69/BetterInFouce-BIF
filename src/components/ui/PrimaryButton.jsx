d6c50f8722f35530b4523dbb1c332bd6d5038456
Mino <minho00123@gmail.com>
2025-08-07 23:42:02 +0900

    refactor: 버튼에 onClick 추가

--- src/components/ui/PrimaryButton.jsx
+++ src/components/ui/PrimaryButton.jsx
@@ -1,6 +1,9 @@
-export default function Button({ title }) {
+export default function Button({ title, onClick }) {
   return (
-    <button className="bg-primary w-full rounded-lg px-2 py-3 font-medium text-white transition-all hover:bg-[#0A7B06]">
+    <button
+      onClick={onClick}
+      className="bg-primary w-full rounded-lg px-2 py-3 font-medium text-white transition-all hover:bg-[#0A7B06]"
+    >
       {title}
     </button>
   );
--- src/components/ui/SecondaryButton.jsx
+++ src/components/ui/SecondaryButton.jsx
@@ -1,6 +1,9 @@
-export default function Button({ title }) {
+export default function Button({ title, onClick }) {
   return (
-    <button className="from-gradient-yellow w-full rounded-lg bg-linear-to-r to-[#b1d827] px-2 py-3 font-medium text-white transition-all hover:bg-[#0A7B06]">
+    <button
+      onClick={onClick}
+      className="from-gradient-yellow w-full rounded-lg bg-linear-to-r to-[#b1d827] px-2 py-3 font-medium text-white transition-all hover:bg-[#0A7B06]"
+    >
       {title}
     </button>
   );
