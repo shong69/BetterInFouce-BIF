a99826bb67f0216ffd957e28298ddd1b49067665
99MotiveU <dongi292@hanmail.net>
2025-08-11 10:03:07 +0900

    feat: 다이어리와 데이터 연결중

--- src/main/java/com/sage/bif/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sage/bif/common/exception/GlobalExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
-import io.swagger.v3.oas.annotations.Hidden;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
