84e4d5485878abd115ac076dc6d30c63ea730816
Mino <minho00123@gmail.com>
2025-08-22 12:34:51 +0900

    style: 주석 제거

--- src/main/java/com/sage/bif/todo/event/listener/UserWithdrawalListener.java
+++ src/main/java/com/sage/bif/todo/event/listener/UserWithdrawalListener.java
@@ -27,23 +27,14 @@ public class UserWithdrawalListener {
         try {
             Long bifId = event.getBifId();
 
-            // 연관 테이블들을 순서대로 삭제 (외래키 제약 조건 고려)
-            
-            // 1. RoutineCompletion 삭제 (Todo와 연관)
             int deletedRoutineCompletions = routineCompletionRepository.deleteByTodo_BifUser_BifId(bifId);
-            
-            // 2. SubTodo 삭제 (Todo와 연관)  
+
             int deletedSubTodos = subTodoRepository.deleteByTodo_BifUser_BifId(bifId);
-            
-            // 3. todo_repeat_days 삭제 (Todo의 ElementCollection)
-            // JPA에서 @ElementCollection은 부모 엔티티 삭제 시 자동 삭제되지만
-            // 명시적으로 삭제하는 경우를 위해 네이티브 쿼리 사용
+
             int deletedRepeatDays = todoRepository.deleteRepeatDaysByBifId(bifId);
-            
-            // 4. Todo 엔티티 삭제 (메인 테이블)
+
             int deletedTodos = todoRepository.deleteByBifUser_BifId(bifId);
-            
-            // 5. WebPushSubscription 삭제 (Bif와 연관)
+
             webPushSubscriptionRepository.deleteByBif_BifId(bifId);
 
             log.info("회원 탈퇴로 모든 사용자 데이터 삭제 완료: bifId={}, 할일={}개, 서브할일={}개, 루틴완료기록={}개, 반복요일={}개, 웹푸시구독 삭제완료", 
