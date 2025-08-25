e367d86bb14a91c5b911a1c59f4297ce18d9eff4
shong69 <shong7576@gmail.com>
2025-08-26 01:54:33 +0900

    fix: 회원 탈퇴 컴포넌트 명명&canWriteToday 설정 변경

--- src/main/java/com/sage/bif/diary/event/listener/UserWithdrawalListener.java
+++ src/main/java/com/sage/bif/diary/event/listener/UserWithdrawalListener.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
-@Component
+@Component("diaryUserWithdrawalListener")
 @RequiredArgsConstructor
 public class UserWithdrawalListener {
     
